141342ecde8c5e297c07e85f2b254a036d3d26c9
DiemHang05 <tdhangdesigner@gmail.com>
2024-08-11 19:26:20 +0700

    Add files via upload
    Viết bổ sung các hàm vào chương trình xử lý mảng 1 chiều các hàm thực hiện những yêu cầu sau:
    -Viết hàm sắp xếp tăng theo PP interchange sort cho dữ liệu số nguyên/số thực/ký tự/ chuỗi ký tự.
    -Viết hàm sắp xếp giảm theo PP interchange sort cho dữ liệu số nguyên/số thực/ký tự/ chuỗi ký tự.

---
+++ Project3/Project3/Source.cpp
@@ -0,0 +1,29 @@
+﻿// Hàm sắp xếp tăng theo phương pháp Interchange Sort
+template <typename T>
+void interchange_sort_asc(T arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] > arr[j]) {
+				// Hoán đổi
+				T temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
+
+// Hàm sắp xếp giảm theo phương pháp Interchange Sort
+template <typename T>
+void interchange_sort_desc(T arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] < arr[j]) {
+				// Hoán đổi
+				T temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
